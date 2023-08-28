d68d071f219b454f4636e5cf311efc0dc1ce913a
Kirra Hyde <kirra.hyde@gmail.com>
2023-08-28 17:47:13 -0400

    WIP

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,20 +1,30 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import Snacks from "./Snacks";
 
 const defaultSnacks = ["chips", "coke", "candy-bars"];
 
 function VendingMachine({ snacks = defaultSnacks }) {
 
+  const [snack, setSnack] = useState("");
+
+  function handleChange(evt) {
+    setSnack(evt.target.value);
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    Navigate(`/snacks/{snack}`);
+  }
 
   return (
     <div>
       <h1>Vending Machine</h1>
       <ul>
         {snacks.map(snack => (
-          <li key={snack}>
-            <Link to={`/snacks/${snack}`}>Show me {snack}!</Link>
-          </li>
+          <form key={snack} value={snack} onChange={handleChange} onSubmit={handleSubmit} >
+            <button>Get {snack}!</button>
+          </form>
         ))}
       </ul>
     </div>
